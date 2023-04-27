0cd513b88e31b9c537d57de6793da2b1f4036723
HaekyungSeong <haekyungs95@gmail.com>
2023-04-28 08:54:52 +0900

    홀수용 엘리베이터 모듈설계

---
+++ oddElevator.js
@@ -0,0 +1,9 @@
+function oddElevator(floorNumber){
+  if(floorNumber === 10 || floorNumber === -1){
+    return console.log(`${floorNumber}층 입니다.`)
+  }else if(floorNumber%2===1){
+    return console.log(`${floorNumber}층 입니다.`)
+  }else{
+    return console.log('해당 엘리베이터는 홀수층만 이동 가능하십니다.')
+  }
+}
