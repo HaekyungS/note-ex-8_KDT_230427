35334324aa0433a85c16dacd40e265637f34d176
HaekyungSeong <haekyungs95@gmail.com>
2023-04-28 08:54:43 +0900

    짝수용 엘리베이터 모듈설계

---
+++ evenElevator.js
@@ -0,0 +1,9 @@
+function evenElevator(floorNumber){
+  if(floorNumber === 1 || floorNumber === -1){
+    return console.log(`${floorNumber}층 입니다.`)
+  }else if(floorNumber%2===0){
+    return console.log(`${floorNumber}층 입니다.`)
+  }else{
+    return console.log('해당 엘리베이터는 짝수층만 이동 가능하십니다.')
+  }
+}
