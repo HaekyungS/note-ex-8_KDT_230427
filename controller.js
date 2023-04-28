ea259f6e885cc6c9749ee503483482ad45a15c4c
HaekyungSeong <haekyungs95@gmail.com>
2023-04-28 09:50:20 +0900

    일부 문구 및 콘솔 수정

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer"
-import evenElevator from "./evenElevator.js"
-import oddElevator from "./oddElevator.js"
+import evenElevator from "./models/evenElevator.js"
+import oddElevator from "./models/oddElevator.js"
 import Choice from "inquirer/lib/objects/choice.js"
 
 inquirer
@@ -8,7 +8,7 @@ inquirer
     {
       type: "list",
       name: "chooseElevator",
-      message: "어느 엘리베이터를 타시겠습니까?",
+      message: "엘리베이터를 골라주세요. (even 은 짝수, odd는 홀수)",
       choices : [evenElevator, oddElevator]
     },
     {
@@ -18,10 +18,13 @@ inquirer
       choices: [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }])
   .then((answers) => {
-    console.log(answers)
+    // console.log(answers)
+    console.log('문이 열렸습니다.')
+    console.log(`${answers.floornumber}층을 누르셨습니다.`)
     if(answers.chooseElevator==='evenElevator'){
       evenElevator(answers.floornumber)
     }else{
       oddElevator(answers.floornumber)
     }
+    console.log('문이 닫힙니다.')
   })
