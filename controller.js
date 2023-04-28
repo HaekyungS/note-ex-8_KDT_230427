ee0660ef92cbebdac4663a9280a45800d93d08a4
HaekyungSeong <haekyungs95@gmail.com>
2023-04-28 09:21:49 +0900

    controller 에 export 추가 및 모듈 적용

--- controller.js
+++ controller.js
@@ -1,11 +1,27 @@
 import inquirer from "inquirer"
+import evenElevator from "./evenElevator.js"
+import oddElevator from "./oddElevator.js"
+import Choice from "inquirer/lib/objects/choice.js"
 
 inquirer
-  .prompt([{
-    type: "list",
-    name: "floornumber",
-    message: [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  }])
-  .then((answers)=>{
-    console.log()
+  .prompt([
+    {
+      type: "list",
+      name: "chooseElevator",
+      message: "어느 엘리베이터를 타시겠습니까?",
+      choices : [evenElevator, oddElevator]
+    },
+    {
+      type: "list",
+      name: "floornumber",
+      message: "몇층을 가시나요?",
+      choices: [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }])
+  .then((answers) => {
+    console.log(answers)
+    if(answers.chooseElevator==='evenElevator'){
+      evenElevator(answers.floornumber)
+    }else{
+      oddElevator(answers.floornumber)
+    }
   })
