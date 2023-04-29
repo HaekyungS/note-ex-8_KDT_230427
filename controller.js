beadcad0c9d5cffdf2b189bf93bb139a7ba09724
HaekyungSeong <haekyungs95@gmail.com>
2023-04-29 22:36:47 +0900

    async 추가하여 시간설정

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ inquirer
       type: "list",
       name: "chooseElevator",
       message: "엘리베이터를 골라주세요. (even 은 짝수, odd는 홀수)",
-      choices : [evenElevator, oddElevator]
+      choices: [evenElevator, oddElevator]
     },
     {
       type: "list",
@@ -19,12 +19,36 @@ inquirer
     }])
   .then((answers) => {
     // console.log(answers)
-    console.log('문이 열렸습니다.')
-    console.log(`${answers.floornumber}층을 누르셨습니다.`)
-    if(answers.chooseElevator==='evenElevator'){
-      evenElevator(answers.floornumber)
-    }else{
-      oddElevator(answers.floornumber)
+    async function elevator(answers) {
+      console.log('문이 열렸습니다.')
+      console.log(`${answers.floornumber}층을 누르셨습니다.`)
+
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('문이 닫힙니다. 올라가겠습니다.')
+          resolve()
+        }, 1000)
+      })
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              if (answers.chooseElevator === 'evenElevator') {
+                evenElevator(answers.floornumber)
+              } else {
+                oddElevator(answers.floornumber)
+              }
+              resolve()
+            }, answers.floornumber * 1000)
+          })
+        })
+        .then(() => {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              console.log('문이 닫힙니다.')
+              resolve()
+            }, 1000)
+          })
+        })
     }
-    console.log('문이 닫힙니다.')
+    elevator(answers)
   })
