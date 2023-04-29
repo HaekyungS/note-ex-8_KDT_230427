cb3855c866c580fabdfa3d7be2cd79ebb7ef5254
HaekyungSeong <haekyungs95@gmail.com>
2023-04-29 22:42:21 +0900

    async 빼고 promise 만으로 수정

--- controller.js
+++ controller.js
@@ -19,36 +19,38 @@ inquirer
     }])
   .then((answers) => {
     // console.log(answers)
-    async function elevator(answers) {
-      console.log('문이 열렸습니다.')
-      console.log(`${answers.floornumber}층을 누르셨습니다.`)
+    // async function elevator(answers) {
+    console.log('문이 열렸습니다.')
+    console.log(`${answers.floornumber}층을 누르셨습니다.`)
 
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          console.log('문이 닫힙니다. 올라가겠습니다.')
-          resolve()
-        }, 1000)
+    // await 
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('문이 닫힙니다. 올라가겠습니다.')
+        resolve()
+      }, 1000)
+    })
+      .then(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (answers.chooseElevator === 'evenElevator') {
+              evenElevator(answers.floornumber)
+            } else {
+              oddElevator(answers.floornumber)
+            }
+            resolve()
+          }, answers.floornumber * 1000)
+        })
       })
-        .then(() => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              if (answers.chooseElevator === 'evenElevator') {
-                evenElevator(answers.floornumber)
-              } else {
-                oddElevator(answers.floornumber)
-              }
-              resolve()
-            }, answers.floornumber * 1000)
-          })
+      .then(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            console.log('문이 닫힙니다.')
+            resolve()
+          }, 1000)
         })
-        .then(() => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              console.log('문이 닫힙니다.')
-              resolve()
-            }, 1000)
-          })
-        })
-    }
-    elevator(answers)
-  })
+      })
+  }
+    // elevator(answers)
+    // }
+  )
